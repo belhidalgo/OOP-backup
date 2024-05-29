824da76a159fa1e6dfd688c1c4bbc039b36d5053
Belén <b.hidalgo.odria@student.rug.nl>
2024-05-29 11:21:54 +0200

    Comments added

--- 3_rts/src/main/java/nl/rug/oop/rts/util/controller/MapController.java
+++ 3_rts/src/main/java/nl/rug/oop/rts/util/controller/MapController.java
@@ -1,7 +1,7 @@
 package nl.rug.oop.rts.util.controller;
 
 /**
- * Controller.
+ * Controller - get the user's input and use the model to handle that input.
  */
 public class MapController {
 }
---
+++ 3_rts/src/main/java/nl/rug/oop/rts/util/observer/mapObserver.java
@@ -0,0 +1,7 @@
+package nl.rug.oop.rts.util.observer;
+
+/**
+ * Observer - connection between the model and the view.
+ */
+public class mapObserver {
+}
